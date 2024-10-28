487c723164f812b7a060be84f138847274cd4b85
thejopu <63508176+thejopu@users.noreply.github.com>
2024-10-28 20:02:29 +0000

    website overhaul

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     loadItems();
     loadTime();
-    addSearchEvents();
-    // Note-related functions removed
+    setInterval(loadTime, 1000); // Update time every second
 });
 
 function loadItems() {
@@ -15,13 +14,13 @@ function loadItems() {
         },
         {
             "icon": "fa-brands fa-discord",
-            "text": "Discord Analytics",
+            "text": "Wizarding Cards Analytics",
             "link": "https://discordanalytics.xyz/dash",
             "accent": "#8b89cc"
         },
         {
             "icon": "fa-solid fa-terminal",
-            "text": "WC Console",
+            "text": "Wizarding Cards Console",
             "link": "https://logs.jdf.gg",
             "accent": "#8b89cc"
         }
@@ -49,37 +48,27 @@ function addItemEvents(items) {
             div.style.color = items[index].accent;
         });
         div.addEventListener("mouseout", function(event) {
-            div.style.color = "#111";
+            div.style.color = "#cccccc";
         });
     });
 }
 
 function loadTime() {
-    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; // Corrected the day abbreviation
-
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const currentDate = new Date();
+    
     const hours = currentDate.getHours();
     const minutes = currentDate.getMinutes();
-    const formattedTime = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+    const formattedTime = `${hours < 10 ? '0' : ''}${hours}<span class="colon">:</span>${minutes < 10 ? '0' : ''}${minutes}`;
 
-    // Format date to dd/mm/yyyy
     const day = String(currentDate.getDate()).padStart(2, '0');
     const month = String(currentDate.getMonth() + 1).padStart(2, '0');
     const year = currentDate.getFullYear();
     const formattedDate = `${day}/${month}/${year}`;
 
-    document.querySelector('.time').textContent = formattedTime;
+    document.querySelector('.time').innerHTML = formattedTime;  // Set HTML instead of textContent for colon
     document.querySelector(".day").textContent = weekday[currentDate.getDay()];
     document.querySelector("#date").textContent = formattedDate;
 }
 
 
-function addSearchEvents() {
-    document.querySelector(".search").addEventListener("keyup", function(event) {
-        var query = event.target.value.trim();
-        if (event.keyCode == 13) {
-            var url = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
-            window.location = url;
-        }
-    });
-}
